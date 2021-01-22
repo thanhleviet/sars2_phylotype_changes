f2a0a8eb8a414254ef562eb799acc478b38ccab5
thanhleviet <thanh.le-viet@quadram.ac.uk>
2021-01-22 20:18:58 +0000

    fix missing global.r

---
+++ app/global.r
@@ -0,0 +1,56 @@
+library(readr)
+library(dplyr, warn.conflicts = FALSE)
+library(tidyr)
+library(ggplot2)
+library(lubridate)
+library(stringr)
+
+plotly_height <- 800
+
+aa_changes <- read_rds("data/data.rds")
+
+phylotype_collecting_org <- aa_changes %>% 
+  mutate(phylotype = paste0(lineage,"-",phylotype))
+
+spike <- aa_changes %>% 
+  mutate(phylotype = paste0(lineage, "-", phylotype))
+
+input_data <- aa_changes %>%
+  filter(!is.na(phylotype)) %>%
+  mutate(phylotype = paste0(lineage,"-",phylotype)) %>%
+  group_by(phylotype, sample_date) %>%
+  summarise(count = n()) %>%
+  ungroup() %>%
+  arrange(sample_date) %>%
+  group_by(phylotype) %>%
+  mutate(cumsum = cumsum(count)) %>%
+  mutate(days = n())
+
+# Sort phylotype by their frequency
+phylotypes <- input_data %>% 
+  group_by(phylotype) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count)) %>% 
+  pull(phylotype)
+
+
+# spike <- aa_changes %>%
+#   group_by(phylotype, sample_date, variants) %>% 
+#   summarise(count = n()) %>% 
+#   ungroup() %>% 
+#   arrange(sample_date) %>% 
+#   group_by(phylotype) %>% 
+#   mutate(cumsum = cumsum(count)) %>% 
+#   mutate(days = n()) %>% 
+#   ungroup() %>% 
+#   separate_rows(variants, convert = TRUE, sep = "\\|") %>% 
+#   distinct_all() %>% 
+#   filter(grepl("S:",variants)) %>% 
+#   mutate(variants = gsub("S:","",variants))
+
+# phylotypes_update <- b117 %>%
+#   filter(days >= 3) %>%
+#   group_by(phylotype) %>%
+#   summarise(count = n()) %>%
+#   arrange(desc(count)) %>%
+#   pull(phylotype)
